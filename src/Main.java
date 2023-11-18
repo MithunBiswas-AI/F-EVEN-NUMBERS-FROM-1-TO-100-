b9a23758fed355e15d5761a2045de933fe4f6282
Programmer <mithunatom1@gmail.com>
2023-11-18 16:05:52 +0100

    F EVEN NUMBERS FROM 1 TO 100

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+
+        for (int i = 2; i <= 100; i += 2)
+        {
+            sum += i;
+        }
+
+                System.out.println("Sum of even numbers from 1 to 100: " + sum);
+
+    }
+}
